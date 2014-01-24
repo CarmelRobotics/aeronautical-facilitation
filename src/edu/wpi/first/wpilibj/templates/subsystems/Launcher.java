c3cc0d10dfb239abc4ca729481264011e2bdb555
fletcherstud <fletcherstud@aol.com>
2014-01-23 18:01:59 -0800

    Added code to shoot and started commands.

--- src/edu/wpi/first/wpilibj/templates/subsystems/Launcher.java
+++ src/edu/wpi/first/wpilibj/templates/subsystems/Launcher.java
@@ -2,6 +2,7 @@
 package edu.wpi.first.wpilibj.templates.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.Solenoid;
 
 /**
  *
@@ -9,10 +10,17 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 public class Launcher extends Subsystem {
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
+    Solenoid launcher;
 
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
+    }
+    public Launcher(){
+        launcher = new Solenoid(5);
+    }
+    public void setLauncherSolenoid(boolean on){
+        launcher.set(on);
     }
 }
 
