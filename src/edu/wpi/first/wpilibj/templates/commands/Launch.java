5072963ce5c616848b82559a3d937fc03643430a
fletcherstud <fletcherstud@aol.com>
2014-01-23 19:21:32 -0800

    Few minor code and comment changes to the launcher code.

--- src/edu/wpi/first/wpilibj/templates/commands/Launch.java
+++ src/edu/wpi/first/wpilibj/templates/commands/Launch.java
@@ -1,5 +1,9 @@
 
 package edu.wpi.first.wpilibj.templates.commands;
+import edu.wpi.first.wpilibj.templates.OI;
+import edu.wpi.first.wpilibj.templates.RobotMap;
+import edu.wpi.first.wpilibj.templates.subsystems.Launcher;
+
 
 /**
  *
