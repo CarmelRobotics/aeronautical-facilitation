3b8217c066ac2fc6b4c4dd6c5c225ad058bc77a1
toastertaster <toastertaster@gmail.com>
2014-01-23 21:10:16 -0800

    Updating the copyright message in all source code files.

--- src/edu/wpi/first/wpilibj/templates/commands/CommandBase.java
+++ src/edu/wpi/first/wpilibj/templates/commands/CommandBase.java
@@ -1,3 +1,10 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2014 FIRST Team 2035. All Rights Reserved.                  */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
 package edu.wpi.first.wpilibj.templates.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
