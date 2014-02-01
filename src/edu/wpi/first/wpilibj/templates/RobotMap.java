44a9be545c419cb6b35228cbfd735701b6ed016a
TheTimeLord <brianandrus2@gmail.com>
2014-02-01 12:13:52 -0800

    Added compressor. Fixed #24

--- src/edu/wpi/first/wpilibj/templates/RobotMap.java
+++ src/edu/wpi/first/wpilibj/templates/RobotMap.java
@@ -42,13 +42,18 @@ public class RobotMap {
     public static final int RollerMotor = 5;
 
     public static final int BlockerMotor = 6;
+    
+    /** The pressure switch that provides feedback to the compressor to turn off when the system reaches 60 PSI. */
+    public static final int pressureSwitch = 13;// fake port. Find real port number
+    /** The compressor is connected to the Digital SideCar Relay */
+    public static final int compressorRelay = 6;//Also fake port.
 
     //public static final int AnalogSidecar = 1; // Analog sidecar possibly connected to cRIO Slot 1, check later
     //public static final int DigitalSidecar = 2; // Digital possibly connected to Slot 2, check later
     //public static final int DriveTrainGyroInput = 1; // Gyro Analog input is on channel 1
     //private static boolean JoystickEnabled = true;
     /**
-     *
+     *Joystick objects for driver joystick
      */
     public static final int DriverJoystickNumber = 1;
 
