e09f7fb7fda90774796321b0a45844e61fde39f3
TheTimeLord <brianandrus2@gmail.com>
2014-02-15 10:02:20 -0800

    added comments to clarify lines of code

--- src/edu/wpi/first/wpilibj/templates/RobotMap.java
+++ src/edu/wpi/first/wpilibj/templates/RobotMap.java
@@ -22,14 +22,14 @@ public class RobotMap {
     /*
      * DIGITAL SIDECAR PWMS NUMBERING (can be 1-10)
      */
-    public static final int FLeftMotorPWM = 1;
-    public static final int BLeftMotorPWM = 2;
-    public static final int FRightMotorPWM = 3;
-    public static final int BRightMotorPWM = 4;
-    public static final int RollerMotorPWM = 5;
-    public static final int MRightMotorPWM = 6;
-    public static final int MLeftMotorPWM = 7;
-    public static final int BlockerMotorPWM = 8;
+    public static final int FLeftMotorPWM = 1; // Front Left drive motor is wired to PWM out 1
+    public static final int BLeftMotorPWM = 2; // Rear Left drive motor is wired to PWM out 2
+    public static final int FRightMotorPWM = 3; // Front Right drive motor is wired to PWM out 3
+    public static final int BRightMotorPWM = 4; // Rear Right drive motor is wired to PWM out 4 
+    public static final int RollerMotorPWM = 5; // Roller motor is wired to PWM out 5
+    public static final int MRightMotorPWM = 6; // Middle Right drive motor is set to PWM out 6
+    public static final int MLeftMotorPWM = 7; // Middle Left drive motor is set to PWM out 7
+    public static final int BlockerMotorPWM = 8; 
 
     /*
      * DIGITAL SIDECAR DIGITAL INPUTS NUMBERING (can be 1-14)
@@ -45,27 +45,27 @@ public class RobotMap {
     /*
      * SOLENOID RELAY CARTRIDGE NUMBERING (can be 1-8)
      */
-    public static final int LaunchRightSolenoid = 2;
-    public static final int DriveTrainHighGearSolenoid = 3;
-    public static final int DriveTrainLowGearSolenoid = 4;
+    public static final int LaunchRightSolenoid = 2; // Right Launcher solenoid is set to relay cartridge 2
+    public static final int DriveTrainHighGearSolenoid = 3; // Drive Train High Gear is set to relay cartridge 3
+    public static final int DriveTrainLowGearSolenoid = 4; // Drive Train Low Gear is set to relay cartridge 4
 
-    public static final int LaunchLeftSolenoid = 6;
-    public static final int RollerExtensionSolenoid = 7;
-    public static final int RollerRetractSolenoid = 8;
+    public static final int LaunchLeftSolenoid = 6; // Left Launcher solenoid is set to relay cartridge 6
+    public static final int RollerExtensionSolenoid = 7; // Roller extension solenoid relay cartridge 7
+    public static final int RollerRetractSolenoid = 8; // Roller retraction solenoid is set to 8
 
     /*
      * BLOCKER SUBSYSTEM CONSTANTS
      */
-    public static final Relay.Value RaiseBlockerMotor = Relay.Value.kForward;
+    public static final Relay.Value RaiseBlockerMotor = Relay.Value.kForward; 
     public static final Relay.Value LowerBlockerMotor = Relay.Value.kReverse;
     public static final Relay.Value blocker = Relay.Value.kOff;
 
     /*
      * ROLLER SUBSYSTEM CONSTANTS
      */
-    public static final double RollerRetrievalMotorSpeed = -1.0;
-    public static final double RollerReleaseMotorSpeed = 1.0;
-    public static final double RollerOffMotorSpeed = 0.0;
+    public static final double RollerRetrievalMotorSpeed = -1.0; // Roller motor retreive speed is set to reverse 100%
+    public static final double RollerReleaseMotorSpeed = 1.0; // Roller motor release speed is set to forward 100%
+    public static final double RollerOffMotorSpeed = 0.0; // Roller motor stationary is set to 0%
 
     /*
      * LAUNCHER SUBSYSTEM CONSTANTS
@@ -77,7 +77,7 @@ public class RobotMap {
      * DRIVETRAIN SUBSYSTEM CONSTANTS
      */
     public static final boolean DriveTrainLowGearSolenoidValue = false;
-    public static final double AutonomousSpeed = 0.40;
+    public static final double AutonomousSpeed = 0.40; // Autonomous speed set to 40%
 
     /*
      * The Joystick USB number (can be 1-4)
@@ -90,13 +90,13 @@ public class RobotMap {
     public static final Joystick DriverJoystick = new Joystick(DriverJoystickUSBNumber);
     public static final Joystick AlternateJoystick = new Joystick(2);
 
-    public static final JoystickButton ButtonDriveTrainShiftToLowGear = new JoystickButton(DriverJoystick, 1);
-    public static final JoystickButton ButtonRollerRetrieveBall = new JoystickButton(DriverJoystick, 2);
-    public static final JoystickButton ButtonRollerReleaseBall = new JoystickButton(DriverJoystick, 3);
-    public static final JoystickButton ButtonRollerExtend = new JoystickButton(DriverJoystick, 4);
-    public static final JoystickButton ButtonRollerRetract = new JoystickButton(DriverJoystick, 5);
-    public static final JoystickButton ButtonLaunchBall = new JoystickButton(DriverJoystick, 6);
-    public static final JoystickButton ButtonPassBall = new JoystickButton(DriverJoystick, 7);
+    public static final JoystickButton ButtonDriveTrainShiftToLowGear = new JoystickButton(DriverJoystick, 1); // Hold button 1 to shift lower gear
+    public static final JoystickButton ButtonRollerRetrieveBall = new JoystickButton(DriverJoystick, 2); // Hold button 2 to activate roller
+    public static final JoystickButton ButtonRollerReleaseBall = new JoystickButton(DriverJoystick, 3); // Hold button 3 to recerse roller
+    public static final JoystickButton ButtonRollerExtend = new JoystickButton(DriverJoystick, 4); // Hold button 4 to extend roller
+    public static final JoystickButton ButtonRollerRetract = new JoystickButton(DriverJoystick, 5); // Hold button 5 to retract roller
+    public static final JoystickButton ButtonLaunchBall = new JoystickButton(DriverJoystick, 6); // Hold button 6 to launch ball # 2 solenoids
+    public static final JoystickButton ButtonPassBall = new JoystickButton(DriverJoystick, 7); // Hold button 7 to pass ball # 1 solenoid
 
     //public static final JoystickButton dButtonBlkD = new JoystickButton(dStick, 2);
     //public static final JoystickButton dButtonBlkU = new JoystickButton(dStick, 3);
