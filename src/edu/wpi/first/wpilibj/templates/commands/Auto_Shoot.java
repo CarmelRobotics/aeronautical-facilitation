05cb868da13b2ebdfb1894ff40c47aec0e163d82
TheTimeLord <brianandrus2@gmail.com>
2014-02-15 14:24:22 -0800

    added Auto_Shoot code for Autonomous2

--- src/edu/wpi/first/wpilibj/templates/commands/Auto_Shoot.java
+++ src/edu/wpi/first/wpilibj/templates/commands/Auto_Shoot.java
@@ -6,18 +6,39 @@
 /*----------------------------------------------------------------------------*/
 package edu.wpi.first.wpilibj.templates.commands;
 
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+
 /**
  *
  * @author Team 2035
  */
-public class ExampleCommand extends CommandBase {
 
-    /**
-     *
+public class Auto_Shoot extends CommandGroup {
+
+    private Timer time;
+   /** BRANCH 1:
+     * 1: wait .2 sec
+     * 2: extend roller
+     * 3: wait .35 sec
+     * 4: launch ball
+     * 5: wait .35 sec
+     * 6: retract roller
+     * BRANCH 2:
+     * 1: turn to search for target in range
+     * 2: when target is found, align properly
+     * 3: #GOTO BRANCH 1
      */
-    public ExampleCommand() {
+    public Auto_Shoot() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
+        addSequential(new Pause(.2));
+        addSequential(new RollerExtend());
+        addSequential(new Pause(.35));
+        addSequential(new Launch());
+        addSequential(new Pause(.35));
+        addSequential(new RollerRetract());
     }
 
     // Called just before this Command runs the first time
