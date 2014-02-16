4c7372e3df5673e35681b96bf9006e214a333d3c
Harrisonwhitaker <harrisonwhitaker@yahoo.com>
2014-02-16 15:34:04 -0800

    Added the Ultrasonic class

---
+++ src/edu/wpi/first/wpilibj/templates/subsystems/Ultrasonic.java
@@ -0,0 +1,23 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2014 FIRST Team 2035. All Rights Reserved.                  */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+package edu.wpi.first.wpilibj.templates.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ * @author Team 2035
+ */
+public class ExampleSubsystem extends Subsystem {
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
